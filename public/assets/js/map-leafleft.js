150144be909f5375e8a7392cf2f619f05cbfbb02
Ahmed elazhary <ahmedelazhary804@gmail.com>
2026-08-06 19:17:40 +0300

    security: remove exposed Mapbox token - replace with placeholder

--- public/assets/js/map-leafleft.js
+++ public/assets/js/map-leafleft.js
@@ -14,7 +14,7 @@ $(function() {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
 		id: 'mapbox.streets'
 	}).addTo(mymap2);
-	L.marker([51.5, -0.09]).addTo(mymap2).bindPopup("<b>Hello world!<\/b><br />I am a popup.").openPopup();
+	L.marker([51.5, -0.09]).addTo(mymap2).bindPopup("<b>Hello world!<\/b><br \/>I am a popup.").openPopup();
 	// Adding a Circle
 	var mymap3 = L.map('leaflet3').setView([51.505, -0.09], 13);
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=YOUR_MAPBOX_TOKEN_HERE', {
